48452390b0979a49d2500316ad562c015207dcc5
Gonzalo_Dip <dipgonzalo2@gmail.com>
2024-09-15 18:52:48 -0300

    Tareas realizadas:
    1) Modificaciones en el gitignore.
    2) Agregada la funcionalidad de que el bot replique el mensaje escrito por los usuarios
    3) Creado el Repositorio de Código Prueba para almacenar funcionalidades extras

--- src/main/java/com/relaxingleg/Listeners.java
+++ src/main/java/com/relaxingleg/Listeners.java
@@ -3,6 +3,8 @@ package com.relaxingleg;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
@@ -10,19 +12,15 @@ import org.jetbrains.annotations.NotNull;
 public class Listeners extends ListenerAdapter {
 
     @Override
-    public void onReady(@NotNull ReadyEvent event) {
+    public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+        if (event.getAuthor().isBot()) return;  //Evitamos que el bot se auto responda
+        MessageChannel channel = event.getChannel();
+        channel.sendMessage(event.getMessage().getContentRaw()).queue();
 
-            JDA jda =event.getJDA();
-            for (Guild guild : jda.getGuilds()) {
-
-                for(TextChannel channel : guild.getTextChannels()) {
-
-                    channel.sendMessage("Prueba 2 antes de commit and push").queue();;
-                }
-
-        }
 
     }
+
+
 
 
 }
