7bb40c314fff1dfb2ded11dcaa982840721da473
Gonzalo_Dip <dipgonzalo2@gmail.com>
2024-09-15 10:02:10 -0300

    Configurando el main (sin Token del Bot)

--- src/main/java/com/relaxingleg/Main.java
+++ src/main/java/com/relaxingleg/Main.java
@@ -1,8 +1,14 @@
 package com.relaxingleg;
 
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import javax.security.auth.login.LoginException;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        JDA jda = JDABuilder.createDefault("").build();
 
 
     }
