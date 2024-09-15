33dfad50683845783ba7798ae20a53818b15bed5
Gonzalo_Dip <dipgonzalo2@gmail.com>
2024-09-15 10:27:03 -0300

    Configuración plantilla Listeners.

---
+++ src/main/java/com/relaxingleg/Listeners.java
@@ -0,0 +1,15 @@
+package com.relaxingleg;
+
+import net.dv8tion.jda.api.events.session.ReadyEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+public class Listeners extends ListenerAdapter {
+
+    @Override
+    public void onReady(@NotNull ReadyEvent event) {
+
+    }
+
+
+}
