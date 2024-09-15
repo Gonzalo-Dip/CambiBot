4fb0eed4e801c28e42601120085570ced907d6fa
Gonzalo_Dip <dipgonzalo2@gmail.com>
2024-09-15 16:14:26 -0300

    Resultado exitoso de implementar el mensaje de bienvenida del bot al ponerse online

--- src/main/java/com/relaxingleg/Listeners.java
+++ src/main/java/com/relaxingleg/Listeners.java
@@ -1,6 +1,8 @@
 package com.relaxingleg;
 
+import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
@@ -10,7 +12,13 @@ public class Listeners extends ListenerAdapter {
     @Override
     public void onReady(@NotNull ReadyEvent event) {
 
+            JDA jda =event.getJDA();
+            for (Guild guild : jda.getGuilds()) {
 
+                for(TextChannel channel : guild.getTextChannels()) {
+
+                    channel.sendMessage("Prueba 2 antes de commit and push").queue();;
+                }
 
         }
 
