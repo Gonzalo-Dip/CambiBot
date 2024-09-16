9e2667e61fbd03a5a7e0068f25108f104d248490
Gonzalo_Dip <dipgonzalo2@gmail.com>
2024-09-15 22:02:27 -0300

    Agregando un CommandManager con la finalidad de facilitar la interacción del usuario con el bot a través de comandos. Los cambios fueron:
    1) Creación de la clase CommandManager.java
    2) Creación de la interfaz ICommand.java
    3) Modificaciones en el main para entrelazar las nuevas implementaciones

---
+++ src/main/java/com/relaxingleg/ICommand.java
@@ -0,0 +1,19 @@
+package com.relaxingleg;
+
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.interactions.commands.SlashCommandInteraction;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+
+import java.util.List;
+
+public interface ICommand {
+
+    String getName();
+
+    String getDescription();
+
+    List<OptionData> getOptions();
+
+    void execute(SlashCommandInteractionEvent event);
+}
