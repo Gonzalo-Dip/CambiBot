3f9db9d21ef1a3d0f2ddc307feb530b65c93ccc4
Jota <joaquinarpee@gmail.com>
2024-11-22 20:30:10 -0300

    detect implementado a discord

--- src/main/java/com/relaxingleg/Main.java
+++ src/main/java/com/relaxingleg/Main.java
@@ -12,7 +12,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        JDA jda = JDABuilder.createDefault("MTI4MjUwNzI1MDUzOTE3MTg0MA.G_ny7B.i8GF4EAFwlbp5iIw3jYudQ7lIlQeFwzxCDg2As", GatewayIntent.GUILD_MESSAGES, GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MEMBERS, GatewayIntent.GUILD_VOICE_STATES).build();
+        JDA jda = JDABuilder.createDefault("", GatewayIntent.GUILD_MESSAGES, GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MEMBERS, GatewayIntent.GUILD_VOICE_STATES).build();
         jda.addEventListener((new Listeners()));
         CommandManager manager = new CommandManager();
         manager.add(new Sum());
